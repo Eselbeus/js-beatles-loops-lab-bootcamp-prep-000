df1b25687ad4c508b5c8e8f334b5785e87ccc06c
eselbeus <eselbeus@gmail.com>
2018-10-01 07:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesBlay(arrMusicians, arrInstruments){
+ let playArray = [];
+ for (i = 0; i < arrMusicians.length; i++){
+   let currentMusician = arrMusicians[i];
+   let currentInstrument = arrInstruments[i];
+   playArray.push(currentMusician + " plays " + currentInstrument);
+ }
+ return playArray;
+}
